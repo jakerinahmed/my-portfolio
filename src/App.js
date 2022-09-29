52c82ac134e82dad23c1bd2849924137351847f1
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-09-29 12:19:05 +0100

    add About me page and sticky navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css';
 import { NavBar, Headshot } from './Layout'
+import { About } from './pages'
 
 function App() {
   return (
-    <div>
+    <div className='display'>
       <NavBar />
-      <Headshot />
+      <Routes>
+        <Route path='/' element={<Headshot />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
+
   );
 }
 
---
+++ src/pages/About/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const About = () => {
+  return (
+    <>
+    <div className='display'>
+        <h1>A bit about me...</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolorem optio repudiandae sunt maiores nulla laborum, molestias quidem sequi fugit minima nihil rerum deleniti, adipisci modi. Saepe veritatis ut nostrum, facere veniam ea dolorum explicabo vero. Deleniti ratione magnam corrupti repellat eaque ad maxime minus totam, hic similique! Facilis, provident.</p>
+    </div>
+    </>
+  )
+}
+
+export default About
