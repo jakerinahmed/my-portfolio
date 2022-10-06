893d384b046db303283b30b3eff2d48fd723bed4
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-10-06 16:14:23 +0100

    add graduation pic

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,13 +4,29 @@ import './style.css'
 const About = () => {
   return (
     <>
-    <div className='container p-5'>
+      <div className='container p-5'>
         <h1>A bit about me...</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum natus officia eaque dignissimos, sunt velit magni voluptatibus! Repellendus nemo vitae distinctio, quo amet at deleniti explicabo non sapiente facere repudiandae suscipit eum praesentium laboriosam ipsum pariatur libero necessitatibus autem. Enim ea, et atque ipsam optio obcaecati facilis natus provident ad suscipit? Temporibus earum laborum quidem soluta deleniti rem omnis, cumque iure eum? Perferendis dolor eius ut et unde odio sapiente sunt commodi, officiis esse rerum culpa dolorem aliquam maiores impedit nisi! Illo fuga eligendi iste officia quibusdam aliquam, et aspernatur possimus voluptatem reiciendis earum. Fugiat, labore? Maiores aspernatur vitae numquam.</p>
+        
+        <div className='container'>
+          <div className='row'>
+
+          <div className='col-md-6 col-sm-4 text-center'>
+
+            <h1>Before coding</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, voluptatum voluptatibus. Ut modi adipisci quisquam minima mollitia, maxime asperiores quis debitis in fuga accusantium cumque similique ducimus. Dignissimos totam doloribus impedit? Dolorum saepe illo ea sint quae. Ipsa, provident unde animi perspiciatis esse non delectus dolorem eveniet sint nostrum. Minima quidem iure nulla dolore, laudantium consectetur ratione cupiditate saepe, deserunt eaque, possimus iste itaque animi? Ipsam provident ratione enim sed? Velit reiciendis fuga, sunt nisi veniam rerum aliquid qui corporis in maiores eos. Mollitia natus praesentium obcaecati voluptate expedita delectus architecto qui corrupti distinctio excepturi, pariatur saepe harum recusandae quidem?</p>
+          </div>
+
+          <div className='col-md-6 col-sm-4 text-center'>
+            <img className='graduation' src='graduation.png'/>
+          </div>
+
+
+          </div>
+        </div>
 
         <h1>Getting into coding</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas vel voluptatum, fugiat, incidunt animi dignissimos ut corporis doloremque amet consequatur porro repellat. Earum illum voluptatum dolorem temporibus vero sed commodi consequuntur veritatis, sapiente quam blanditiis dignissimos a ratione eum porro fuga quae voluptatem velit! Animi illum cupiditate modi culpa, sapiente aliquam harum maiores non dolor reprehenderit. Animi, porro maiores tempora aliquam atque illo magnam accusamus beatae officia suscipit tenetur libero nam temporibus ex, quis et quas voluptate placeat odit repellat, provident deserunt! Commodi quas corrupti totam atque dignissimos sequi corporis velit? Quae aspernatur assumenda, dolorem ipsam corporis omnis laboriosam nihil!</p>
-    </div>
+      </div>
     </>
   )
 }
