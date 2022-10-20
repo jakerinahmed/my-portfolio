50ad1c8e1975c41e3058784777e637def546c593
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-10-20 16:07:42 +0100

    remove responsive navbar

--- src/Layout/Navbar/index.js
+++ src/Layout/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Nav, Navbar } from 'react-bootstrap'
+// import { Nav, Navbar } from 'react-bootstrap'
 import './style.css'
 
 
@@ -10,17 +10,12 @@ const NavBar = () => {
 
     return (
         <div className="p-3 navbar-main" id="top-of-page">
-            <Navbar collapseOnSelect expand='lg' className="nav">
-                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-                <Navbar.Collapse id='responsive-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <NavLink className={activeClass} to="/" end>Home</NavLink>
-                        <NavLink className={activeClass} to='about'>About me</NavLink>
-                        <NavLink className={activeClass} to="projects">Projects</NavLink>
-                        <NavLink className={activeClass} to="Contacts">Contacts</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <ul className="nav">
+                <NavLink className={activeClass} to="/" end>Home</NavLink>
+                <NavLink className={activeClass} to='about'>About me</NavLink>
+                <NavLink className={activeClass} to="projects">Projects</NavLink>
+                <NavLink className={activeClass} to="Contacts">Contacts</NavLink>
+            </ul>
         </div>
     )
 }
